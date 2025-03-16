b235fac789465b3228d4d0a97a2c7d4b73e614fa
wang <75620398@qq.com>
2025-03-16 23:22:23 +0800

    5

---
+++ test/temp.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         hello world
+// @namespace    http:/un.ysgyx.cn
+// @version      2025-03-16
+// @description  try to take over the world!
+// @author       You
+// @match        https://greasyfork.org/zh-CN/script_versions/new
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=greasyfork.org
+// @grant        none
+// ==/UserScript==
+
+;(function () {
+  "use strict"
+
+  console.log("hello world");
+})()
