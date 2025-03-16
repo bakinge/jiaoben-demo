d34a85efdafd366b111b4e942d5310fe6d4364af
wang <75620398@qq.com>
2025-03-16 23:30:11 +0800

    6

--- test/temp.js
+++ test/temp.js
@@ -1,16 +1,17 @@
 // ==UserScript==
 // @name         hello world
 // @namespace    http:/un.ysgyx.cn
-// @version      2025-03-16
-// @description  try to take over the world!
+// @version      0.1
+// @description  new
 // @author       You
-// @match        https://greasyfork.org/zh-CN/script_versions/new
+// @match        *://*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=greasyfork.org
+// @license       AGPL-3.0
 // @grant        none
 // ==/UserScript==
 
-;(function () {
-  "use strict"
+(function () {
+  "use strict";
 
   console.log("hello world");
-})()
+})();
